16cc41b5939cd4160919abb9d8dc6526b5e20133
Zhansaya <fypdamson@gmail.com>
2026-05-07 21:58:18 +0900

    Add files via upload

---
+++ products.py
@@ -0,0 +1,25 @@
+# products.py
+
+BEACH_PRODUCTS = {
+    "mat": 10, 
+    "beach tube": 15, 
+    "floaty": 20,
+    "umbrella": 25,
+    "life guard": 50, 
+}
+
+SKIN_PRODUCTS = {
+    "sunscreen": 12,
+    "aloe vera gel": 8,
+    "lip balm": 5,
+    "face wash": 15,
+    "body lotion": 20,
+}
+
+CLOTHES_PRODUCTS = {
+    "swimsuits": 15,
+    "shorts": 10,
+    "hats": 8,
+    "towels": 12,
+    "flip flops": 10
+    }
